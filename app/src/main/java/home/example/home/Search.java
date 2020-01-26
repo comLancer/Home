af3003eeb4081f92d3a2466c4dd71d7b231065fb
asmaalriyamia <wsan.alriyami@gmail.com>
2020-01-26 09:53:34 +0400

    First comment

--- app/src/main/java/home/example/home/Search.java
+++ app/src/main/java/home/example/home/Search.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class Search extends Fragment {
-
+    //search pa
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {

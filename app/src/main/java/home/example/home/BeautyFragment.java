fbeaa401f9ea3c9cd12be495780b42754c4f07cf
hajerhamed <38397505+hajerhamed@users.noreply.github.com>
2020-01-26 10:04:54 +0400

    + colorsafa

--- app/src/main/java/home/example/home/BeautyFragment.java
+++ app/src/main/java/home/example/home/BeautyFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 public class BeautyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_beauty_fragment, container, false);
     }
